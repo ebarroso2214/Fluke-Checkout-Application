5d8a05f7f947495d0b95beee622731814feaf610
ebarroso2214 <ebarroso2214@gmail.com>
2023-09-07 16:05:28 -0400

    created class and methods for UI

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import typing
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.QtGui import QIcon
+
+
+class app_window(QMainWindow):
+    def __init__(self):
+        super(app_window, self).__init__()
+        self.setGeometry(1200,300,700,700)
+        self.setWindowTitle("Fluke Checkout")
+        self.setToolTip("Fluke Checkout")
+        self.setWindowIcon(QIcon("checkmark.jpg"))
+        self.app_UI()
+    
+    def app_UI(self):
+        self.lbl_name = QtWidgets.QLabel(self)
+        self.lbl_name.setText("Enter name : ")
+        self.lbl_name.move(50,50)
+
+        self.txt_name = QtWidgets.QLineEdit(self)
+        self.txt_name.move(200,50)
+        self.txt_name.resize(200,32)
+
+        self.btn_save = QtWidgets.QPushButton(self)
+        self.btn_save.setText("Checkout")
+        self.btn_save.clicked.connect(self.clicked)
+        self.btn_save.move(200,100)
+
+        self.lbl_checked_out_by = QtWidgets.QLabel(self)
+        self.lbl_checked_out_by.setText("Not Checked Out")
+        self.lbl_checked_out_by.move(50,80)
+        self.lbl_checked_out_by.resize(200,180)
+
+    def clicked(self):
+        self.lbl_checked_out_by.setText("Checked out by : " + self.txt_name.text())
+
+
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(1200,300,700,700)
-    win.setWindowTitle("Fluke Checkout")
-    win.setWindowIcon(QIcon("checkmark.jpg"))
-    win.setToolTip("Fluke Checkout")
-
-    lbl_name = QtWidgets.QLabel(win)
-    lbl_name.setText("Enter name : ")
-    lbl_name.move(50,50)
-
-    txt_name = QtWidgets.QLineEdit(win)
-    txt_name.move(200,50)
-
-    def clicked(self):
-        print('button clicked')
-        print('name :' + txt_name.text())
-
-    btn_save = QtWidgets.QPushButton(win)
-    btn_save.setText('Save')
-    btn_save.clicked.connect(clicked)
-    btn_save.move(200,130)
-
+    win = app_window()
     win.show()
     sys.exit(app.exec_())
 
